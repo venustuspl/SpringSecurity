76bfada37bf739d5660c0e50dafeede558657cc6
venustuspl <venustus.pl@gmail.com>
2020-04-08 21:41:14 +0200

    1.3

--- src/main/java/pl/venustus/SpringSecurity/Service/UserService.java
+++ src/main/java/pl/venustus/SpringSecurity/Service/UserService.java
@@ -26,15 +26,17 @@ public class UserService {
         appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
         appUser.setRole("ROLE_USER");
         appUserRepo.save(appUser);
-        appUserRepo.save(appUser);
+        sendToken(appUser);
     }
 
 
-    private void sendToken() {
+    private void sendToken(AppUser appUser) {
         String tokenValue = UUID.randomUUID().toString();
         Token token = new Token();
         token.setValue(tokenValue);
+        token.setAppUser(appUser);
         tokenRepo.save(token);
+        String url = "http://localhost:8080?token=" + tokenValue;
 
 
     }
